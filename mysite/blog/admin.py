2adb04bceb60ddf7a39a7421be0258bbb7b8b4d8
12397241 <154056318+12397241@users.noreply.github.com>
2024-02-18 16:20:23 +0300

    added post to admin panel

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostRoy(admin.ModelAdmin):
+    list_display = ['title','slug','author','pulish', 'status']
+    list_filter = ['status', 'created','publish','author']
+    search_fields = ['tetle', 'body']
+    preserve_filters = {'slug':('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
